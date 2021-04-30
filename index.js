c5897743f07b4277b9347737cc128145c26a7acf
iravanilla <i.krzh@yandex.ru>
2021-04-30 14:11:26 +0400

    code style

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 async function getfrom_Server(url) {
 
     try {
-    let response = await fetch(url);
-    return response.json();
+        let response = await fetch(url);
+        return response.json();
     } catch(error) {
         throw new Error ('Ошибка HTTP: ' + error)
     }
-    }
+}
     
-    getfrom_Server('https://jsonplaceholder.typicode.com/todos/1'.then((user) => console.log(user)))
+getfrom_Server('https://jsonplaceholder.typicode.com/todos/1'.then((user) => console.log(user)))
